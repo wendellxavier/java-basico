3347eac2d70721474dac4b2a0a48e187b2c3557f
wendellxavier25 <wendellx25@gmail.com>
2024-07-30 12:07:44 -0300

    teste

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        double saldo = 237.48;
+
+        System.out.println("Por favor digite o numero da Agencia: ");
+        int numero =  scanner.nextInt();
+
+        System.out.println("Digite á agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+
+        System.out.println(" Ola " + nomeCliente + " obrigado por criar umma conta em nosso banco, sua agência é " + agencia + ", conta " +  numero + " e seu saldo " +  saldo + " já está disponivel ");
+        
+
     }
+
+    
 }
